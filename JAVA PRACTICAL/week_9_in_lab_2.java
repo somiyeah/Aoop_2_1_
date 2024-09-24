9bd65e348718582b59b2ec127302fdcbc6b2d78d
somiyeah <2320030204@klh.edu.in>
2024-09-24 13:01:54 +0530

    Update week_9_in_lab_2.java

--- JAVA PRACTICAL/week_9_in_lab_2.java
+++ JAVA PRACTICAL/week_9_in_lab_2.java
@@ -1,3 +1,34 @@
-public class week_9_in_lab_2 {
+import java.util.*;
 
+class Employee {
+    private int id;
+    private String name;
+    private String department;
+    private double salary;
+
+    public Employee(int id, String name, String department, double salary) {
+        this.id = id;
+        this.name = name;
+        this.department = department;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    @Override
+    public String toString() {
+        return id + " " + name + " " + department + " " + salary;
+    }
 }
+
+// Comparator for sorting by salary
